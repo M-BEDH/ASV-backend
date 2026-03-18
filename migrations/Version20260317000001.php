64ebdb12ba6131bea7d78f1b47085da1a643be9f
M-BEDH <https://github.com/M-BEDH>
2026-03-18 16:41:24 +0100

    contrainte CHECK sur le rôle utilisateur

---
+++ migrations/Version20260317000001.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20260317000001 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Ajout contrainte CHECK sur la colonne role de la table users';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql("ALTER TABLE users ADD CONSTRAINT chk_user_role CHECK (role IN ('client', 'veterinaire', 'responsable', 'assistant', 'benevole'))");
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE users DROP CONSTRAINT chk_user_role');
+    }
+}
