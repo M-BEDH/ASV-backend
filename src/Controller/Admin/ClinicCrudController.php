cd3d5b4c8a0591002fca50480f98f5a166558752
M-BEDH <https://github.com/M-BEDH>
2026-04-11 16:01:29 +0200

    disable(Action::NEW)

--- src/Controller/Admin/ClinicCrudController.php
+++ src/Controller/Admin/ClinicCrudController.php
@@ -3,6 +3,8 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Clinic;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
@@ -15,6 +17,11 @@ class ClinicCrudController extends AbstractCrudController
     public static function getEntityFqcn(): string
     {
         return Clinic::class;
+    }
+
+    public function configureActions(Actions $actions): Actions
+    {
+        return $actions->disable(Action::NEW);
     }
 
     public function configureCrud(Crud $crud): Crud
