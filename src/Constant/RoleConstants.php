763004071b5f96d5bfdd518c76c0616b453dacb3
M-BEDH <https://github.com/M-BEDH>
2026-04-16 16:19:20 +0200

    edit role CAN EDIT CLINIC

--- src/Constant/RoleConstants.php
+++ src/Constant/RoleConstants.php
@@ -6,7 +6,7 @@ class RoleConstants
 {
     public const ALL             = ['client', 'veterinaire', 'responsable', 'assistant', 'benevole'];
     public const SUPER_ADMIN     = 'super_admin';
-    public const CAN_EDIT_CLINIC = ['responsable', 'veterinaire'];
+    public const CAN_EDIT_CLINIC = ['responsable'];
     public const CAN_DELETE_USER           = ['responsable', 'veterinaire'];
     public const ASSIGNABLE_BY_RESPONSABLE = ['veterinaire', 'assistant', 'benevole'];
     public const CONSULTATION_VETERINAIRE_ROLES = ['veterinaire', 'responsable'];
