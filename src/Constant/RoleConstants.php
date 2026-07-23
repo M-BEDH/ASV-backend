65b625b74be7236c3a38aaba3255fc6f7658dec6
M-BEDH <https://github.com/M-BEDH>
2026-07-23 15:53:04 +0200

    constantes pour rôle RoleConstants au lieu de chaînes brutes

--- src/Constant/RoleConstants.php
+++ src/Constant/RoleConstants.php
@@ -4,10 +4,16 @@ namespace App\Constant;
 
 class RoleConstants
 {
-    public const ALL             = ['client', 'veterinaire', 'responsable', 'assistant', 'benevole'];
-    public const SUPER_ADMIN     = 'super_admin';
-    public const CAN_EDIT_CLINIC = ['responsable'];
-    public const CAN_DELETE_USER           = ['responsable', 'veterinaire'];
-    public const ASSIGNABLE_BY_RESPONSABLE = ['veterinaire', 'assistant', 'benevole'];
-    public const CONSULTATION_VETERINAIRE_ROLES = ['veterinaire', 'responsable'];
+    public const CLIENT      = 'client';
+    public const BENEVOLE    = 'benevole';
+    public const VETERINAIRE = 'veterinaire';
+    public const RESPONSABLE = 'responsable';
+    public const ASSISTANT   = 'assistant';
+    public const SUPER_ADMIN = 'super_admin';
+
+    public const ALL              = [self::CLIENT, self::VETERINAIRE, self::RESPONSABLE, self::ASSISTANT, self::BENEVOLE];
+    public const CAN_EDIT_CLINIC  = [self::RESPONSABLE];
+    public const CAN_DELETE_USER           = [self::RESPONSABLE, self::VETERINAIRE];
+    public const ASSIGNABLE_BY_RESPONSABLE = [self::VETERINAIRE, self::ASSISTANT, self::BENEVOLE];
+    public const CONSULTATION_VETERINAIRE_ROLES = [self::VETERINAIRE, self::RESPONSABLE];
 }
