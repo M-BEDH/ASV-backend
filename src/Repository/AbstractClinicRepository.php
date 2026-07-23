44387d725b6eecbc17342db6a671276a01363792
M-BEDH <https://github.com/M-BEDH>
2026-07-23 15:08:00 +0200

    generics PHPStan AbstractClinicRepository

--- src/Repository/AbstractClinicRepository.php
+++ src/Repository/AbstractClinicRepository.php
@@ -5,8 +5,15 @@ namespace App\Repository;
 use App\Entity\Clinic;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 
+/**
+ * T est une case vide que chaque sous-classe remplit avec son entité via @extends,
+ * ex: AnimalRepository déclare "@extends AbstractClinicRepository<Animal>" 
+ * Ainsi PHPStan sait que findByClinic() renvoie Animal[] (pas object[] générique).
+ * @template T of object
+ * @extends ServiceEntityRepository<T>
+ */
 abstract class AbstractClinicRepository extends ServiceEntityRepository
 {
-    /** @return array<object> */
+    /** @return T[] */ // return Animal (dans cet exemple)
     abstract public function findByClinic(Clinic $clinic): array;
 }
